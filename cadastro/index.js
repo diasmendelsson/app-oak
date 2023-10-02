25b6627e67e1ebbd77141e787f1da652ed0ebd51
Mendelsson Dias <98241288+diasmendelsson@users.noreply.github.com>
2023-10-02 16:00:09 -0300

    Update index.js

--- cadastro/index.js
+++ cadastro/index.js
@@ -7,7 +7,22 @@ const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
+
+
+app.use((req, res, next) => {
+    // Qualquer endereço pode fazer requisição
+    res.header("Access-Control-Allow-Origin", "*");
+    //Tipos de métodos que a API aceita
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    // Permitir o envio de dados para API
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+
+    // Executar o cors
+    app.use(cors());
+    //Quando não houver erro deve continuar o processamento
+    next();
+
+})
 
 const cadastros = {};
 
